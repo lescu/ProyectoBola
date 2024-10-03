ad0867f86a9eb73fb306a806ce093cde945101d5
lescu <niicolescu@gmail.com>
2024-10-03 21:27:09 +0200

    03/102024
    Salto

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
     Vector3 mover;
-    float velocidad = 10f;
+    float fuerzaSALT = 10f;
+    Rigidbody rb;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+       rb = GetComponent<Rigidbody>();
         
     }
 
@@ -18,9 +22,13 @@ public class Bola : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); // h = 1 (D ó ->) h = -1 (A ó <-), h = 0 (NADA)
         float v = Input.GetAxis("Vertical"); // v = 1 (w ó ->) v = -1 (s ó v), v = 0 (NADA)
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * fuerzaSALT, ForceMode.Impulse);
+        }
         Vector3 mover = new Vector3 (h, 0, v);
         
-        transform.Translate(mover * velocidad * Time.deltaTime);
+        transform.Translate(mover * fuerzaSALT,ForceMode.Impulse);
     }
 }
